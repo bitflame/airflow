15f9ed9fd6f2f1a258a0858099ba72c5011d6baa
BitFlame <monazzam@protonmail.com>
2026-02-24 18:48:50 -0500

    changed the cron to run every 12 hour on the hour

--- dags/executing_cron_catchup_backfill.py
+++ dags/executing_cron_catchup_backfill.py
@@ -26,35 +26,30 @@ def task_c():
     print("TASK C executed!")
 
 
+# cron expression components: min, hour, Day of Month, Month, Year * means every
 with DAG(
     dag_id="cron_catchup_backfill",
     description="Using crons, catchup, and backfill",
     default_args=default_args,
     start_date=pendulum.datetime(2026, 2, 19, tz="UTC"),
-    schedule_interval="0 0 * * *", # cron expression components: min, hour, Day of Month, Month, Year * means every 
+    schedule="0 */12 * * 6,0",
     catchup=True,
 ) as dag:
 
-    taskA = BashOperator(task_id="taskA", bash_command=True)
+    taskA = BashOperator(task_id="taskA", bash_command='echo TASK A has executed!')
 
     taskChoose = PythonOperator(task_id="taskChoose", python_callable=choose_branch)
 
     taskBranch = BranchPythonOperator(task_id="taskBranch", python_callable=branch)
 
-    taskC = PythonOperator(
-        task_id = 'taskC',
-        python_callable=task_c
-    )
+    taskC = PythonOperator(task_id="taskC", python_callable=task_c)
 
-    taskD = BashOperator(
-        task_id = 'taskD',
-        bash_command='echo TASK D has executed!'
-    )
+    taskD = BashOperator(task_id="taskD", bash_command="echo TASK D has executed!")
 
     taskE = EmptyOperator(
-        task_id = 'taskE',
+        task_id="taskE",
     )
 
     taskA >> taskChoose >> taskBranch >> [taskC, taskE]
 
-    taskC >> taskD 
+    taskC >> taskD
